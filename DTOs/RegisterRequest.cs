435ed412d193262cbdcb13cb801bb4f75ddaeccb
Lư Tấn Đạt <95582749+LTDat11@users.noreply.github.com>
2026-02-22 09:53:24 +0700

    feat: add validation attributes for Username and Password in RegisterRequest DTO

--- DTOs/RegisterRequest.cs
+++ DTOs/RegisterRequest.cs
@@ -1,7 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SecureTaskApi.DTOs;
 
 public class RegisterRequest
 {
+    [Required]
+    [MinLength(3)]
+    [MaxLength(50)]
     public string Username { get; set; } = default!;
+
+    [Required]
+    [MinLength(6)]
     public string Password { get; set; } = default!;
 }
