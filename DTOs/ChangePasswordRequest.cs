c1a1c95ad03d1c0f40b208fbbc4406df1de56e0c
Lư Tấn Đạt <95582749+LTDat11@users.noreply.github.com>
2026-03-02 11:15:12 +0700

    feat: create class changepasswordrequest

---
+++ DTOs/ChangePasswordRequest.cs
@@ -0,0 +1,8 @@
+namespace SecureTaskApi.DTOs;
+
+public class ChangePasswordRequest
+{
+    public string CurrentPassword { get; set; } = null!;
+    public string NewPassword { get; set; } = null!;
+    public string ConfirmNewPassword { get; set; } = null!;
+}
